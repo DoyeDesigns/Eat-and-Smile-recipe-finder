f20e8392286785e75291c959220f169cd35813f0
Ogoba David <dvdogoba23@gmail.comm>
2023-06-02 11:33:20 +0100

    Finished all filter functions on searchResult(search-results.html/search-result.js) page

--- src/assets/js/search-results.js
+++ src/assets/js/search-results.js
@@ -1,8 +1,8 @@
 const searchResults = document.querySelector("#results");
 const searchResultButton = document.querySelector("#searchResultButton");
 const numOfResults = document.querySelector("#numOfResults");
-const items = Array.from(results.querySelectorAll(".items"));
-const filters = document.querySelectorAll('.filter');
+const filters = document.querySelectorAll(".filter");
+const filterItem = document.querySelector("#filterItem");
 
 import axios from "axios";
 
@@ -19,7 +19,7 @@ let renderSearchResults = () => {
       try {
         const response = await axios(options);
         const responseData = response.data;
-        localStorage.setItem('responseData', responseData);
+        localStorage.setItem("responseData", JSON.stringify(responseData));
 
         function renderResults() {
           response.data.hits.forEach((hit, index) => {
@@ -66,7 +66,7 @@ function performSearch(searchTerm) {
     try {
       const response = await axios(options);
       const responseData = response.data;
-      localStorage.setItem('responseData', responseData);
+      localStorage.setItem("responseData", JSON.stringify(responseData));
 
       function renderResults() {
         response.data.hits.forEach((hit, index) => {
@@ -117,18 +117,107 @@ searchResultButton.addEventListener("click", () => {
 //to check number of rendered items
 const observer = new MutationObserver(() => {
   const numOfItems = document.querySelectorAll(".items").length;
-  numOfResults.textContent = `${numOfItems} Results`;
+  if (numOfItems === 0 || numOfItems === 1) {
+    numOfResults.textContent = `${numOfItems} Result`;
+  } else {
+    numOfResults.textContent = `${numOfItems} Results`;
+  }
+  
 });
 
 observer.observe(searchResults, { childList: true });
 
 // filters event listener
-filters.forEach(filter =>{
-  filter.addEventListener('click', () => {
-    let foodData = localStorage.getItem("responseData")
-    console.log(foodData)
-    // if () {
-      
-    // }
-  })
-})
+filters.forEach((filter) => {
+  filter.addEventListener("click", (event) => {
+    let filterValue = event.target.getAttribute("data-value"); //to get data-value of clicked filter
+    let foodData = JSON.parse(localStorage.getItem("responseData"));
+
+    const filteredResults = foodData.hits.filter(
+      (hit) => hit.recipe.mealType[0] == filterValue
+    );
+    searchResults.innerHTML = ``;
+
+    if (filteredResults.length > 0) {
+      function renderfilterResults() {
+        filteredResults.forEach((filter, index) => {
+          const itemElement = document.createElement("div");
+          itemElement.classList.add("item");
+
+          itemElement.innerHTML = `<a href="item.html">
+                    <div class="items d-flex align-items-center px-0 mb-3 data-item-index="${index}"">
+                      <img src="${filter.recipe.image}" alt="burgers" width="55" height="61" class="image-fluid">
+                      <p class="ms-3">${filter.recipe.label}</p>
+                    </div>
+                    </a>
+                  `;
+          // Attaching the click event listener to each item element
+          itemElement.addEventListener("click", () => {
+            // Accessing the exact array item using the index
+            const clickedItem = filteredResults[index];
+            localStorage.setItem("clickedItem", JSON.stringify(clickedItem));
+            console.log("Clicked item:", clickedItem);
+          });
+
+          searchResults.appendChild(itemElement);
+        });
+
+        filterItem.innerHTML = `
+        <button id="removeFilter" class="btn  rounded border border-1 border-orange">
+        Remove ${filterValue} filter
+        </button>
+        `;
+      }
+      renderfilterResults();
+
+    } else if (filteredResults.length === 0) {
+      let filterValue = event.target.getAttribute("data-value");
+      searchResults.innerHTML = `
+      <div class="container">
+      <p class="fst-italic">
+      no match found.
+      </p>
+      </div>
+      `;
+
+      filterItem.innerHTML = `
+      <button id="removeFilter" class="btn rounded border border-1 border-orange">
+      Remove ${filterValue} filter
+      </button>
+      `;
+
+    } else {
+      console.log("array length cannot be less than 0");
+    }
+  });
+});
+
+//when no match is found remove remove filter and render saved search results
+filterItem.addEventListener("click", () => {
+  let foodData = JSON.parse(localStorage.getItem("responseData")); //saved search results
+  searchResults.innerHTML = ``;
+
+  foodData.hits.forEach((hit, index) => {
+    const itemElement = document.createElement("div");
+    itemElement.classList.add("item");
+
+    itemElement.innerHTML = `<a href="item.html">
+                <div class="items d-flex align-items-center px-0 mb-3 data-item-index="${index}"">
+                  <img src="${hit.recipe.image}" alt="burgers" width="55" height="61" class="image-fluid">
+                  <p class="ms-3">${hit.recipe.label}</p>
+                </div>
+                </a>
+              `;
+
+    // Attaching the click event listener to each item element
+    itemElement.addEventListener("click", () => {
+      // Accessing the exact array item using the index
+      const clickedItem = foodData.hits[index];
+      localStorage.setItem("clickedItem", JSON.stringify(clickedItem));
+      console.log("Clicked item:", clickedItem);
+    });
+
+    searchResults.appendChild(itemElement);
+  });
+  filterItem.innerHTML = ``; // to remove filter button
+});
